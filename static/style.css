6df562e0372536cf7b9119aeef3ce47aacfdd071
muhammadharis-18 <muhammadharisrasheed5@gmail.com>
2026-01-17 22:45:06 +0500

    Update style.css

---
+++ static/style.css
@@ -0,0 +1,48 @@
+body {
+    background: #0f172a;
+    color: white;
+    font-family: Arial;
+    text-align: center;
+}
+
+h1 {
+    color: #38bdf8;
+}
+
+.box {
+    background: #1e293b;
+    padding: 20px;
+    width: 300px;
+    margin: auto;
+    border-radius: 10px;
+}
+
+input {
+    width: 90%;
+    padding: 10px;
+    margin: 5px;
+}
+
+button {
+    padding: 10px;
+    width: 95%;
+    background: #38bdf8;
+    border: none;
+    margin-top: 10px;
+    cursor: pointer;
+}
+
+.save {
+    background: #22c55e;
+}
+
+table {
+    width: 60%;
+    margin: 20px auto;
+    border-collapse: collapse;
+}
+
+th, td {
+    border-bottom: 1px solid gray;
+    padding: 10px;
+}
