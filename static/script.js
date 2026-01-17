393eecd574f10addebf5b316a0d32d918cb20e21
muhammadharis-18 <muhammadharisrasheed5@gmail.com>
2026-01-17 23:56:26 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,13 +2,10 @@ const firebaseConfig = {
     apiKey: "AIzaSyC6wmZVwGrjFM-fkIY4nRIpXdfCCbXaRh4",
     authDomain: "yumzo-dee28.firebaseapp.com",
     projectId: "yumzo-dee28",
-    storageBucket: "yumzo-dee28.firebasestorage.app",
-    messagingSenderId: "981056674227",
-    appId: "1:981056674227:web:c5d30a8ff1f0a377ea13c6"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();;
+const db = firebase.firestore();
 
 let cart = [];
 let total = 0;
@@ -20,33 +17,19 @@ function addToCart(name, price) {
 }
 
 async function placeOrder() {
-    if (cart.length === 0) return alert("Cart is empty!");
+    if (cart.length === 0) return alert("Please add items!");
 
-    const orderData = {
-        orderNum: Date.now().toString().slice(-4),
+    const order = {
+        orderNum: Math.floor(100 + Math.random() * 900),
         items: cart,
         total: total,
-        source: document.getElementById('order-source').value,
-        payment: document.getElementById('payment-method').value,
+        source: document.getElementById('source').value,
         status: "New",
         createdAt: firebase.firestore.FieldValue.serverTimestamp()
     };
 
-    try {
-        // 1. Send to Firebase (Real-time Kitchen)
-        await db.collection("orders").add(orderData);
-        
-        // 2. Send to Python (Local Backup)
-        await fetch("/save_order", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(orderData)
-        });
-
-        alert("Order Placed!");
-        cart = []; total = 0;
-        document.getElementById('total-display').innerText = 0;
-    } catch (e) {
-        console.error("Error: ", e);
-    }
+    await db.collection("orders").add(order);
+    alert("Order Sent to Kitchen!");
+    cart = []; total = 0;
+    document.getElementById('total-display').innerText = 0;
 }
